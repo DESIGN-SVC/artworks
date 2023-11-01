0400c610905c7e64366a9f6da75dcd5bf65ea604
etercy <suelengbr@gmail.com>
2023-11-01 07:48:08 -0300

    feat: cores tailwind adicionadas

--- apps/zeus/tailwind.config.js
+++ apps/zeus/tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  purge: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          '300': '#63F3FD',
+          '350': '#00D6E3',
+          '400': '#1CE2F4',
+          '500': '#00BED2',
+          '950': '#004650'
+        },
+        gray: {
+          '50':  '#F6F8F9',
+          '100': '#EBEFF3',
+          '200': '#CAD1D7',
+          '500': '#5E6D7C',
+          '900': '#373B42',
+          '950': '#22252A',
+        }
+      },
+    },
   },
   plugins: [],
 }
