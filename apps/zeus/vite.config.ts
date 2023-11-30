1f22667c8bb7e1392da45b610cb88dcf3f4452fe
moises-macedo <moises.macedo@hotmail.com.br>
2023-11-30 15:28:50 -0300

    fix:added paths configuration for imports

--- apps/zeus/vite.config.ts
+++ apps/zeus/vite.config.ts
@@ -1,9 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  envPrefix: "PUBLIC_",
+
+  plugins: [react(), tsconfigPaths()],
+
   server: { host: true, port: 4200 },
   preview: { host: true, port: 4300 },
+
+  build: {
+    assetsInlineLimit: 0,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
+  },
 })
