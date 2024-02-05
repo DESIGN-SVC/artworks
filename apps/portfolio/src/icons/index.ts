c2b3102bf4816551cc64e43da664428fc4c8f4fc
etercy <suelengbr@gmail.com>
2024-02-05 16:40:06 -0300

    feat: added logos.

--- apps/portfolio/src/icons/index.ts
+++ apps/portfolio/src/icons/index.ts
@@ -3,3 +3,5 @@ export { GreenCheck } from "./GreenCheck";
 export { Monitor } from "./Monitor";
 export { Palette } from "./Palette";
 export { PurpleCamera } from "./PurpleCamera";
+export { LogoConcentrixWebhelp } from "./LogoConcentrixWebhelp";
+export { LogoArtworks } from "./LogoArtworks";
