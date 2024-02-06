0b07405deb2ba5f0c6cabf77c0bd040b985598fe
Moises <moises.macedo@hotmail.com.br>
2024-02-06 16:49:21 -0300

    feat: created component

---
+++ apps/portfolio/src/componets/toggleSwitch/ToggleSwitch.stories.tsx
@@ -0,0 +1,9 @@
+import { ToggleSwitch } from "./toggleSwitch";
+import { StoryObj } from "@storybook/react";
+
+export default {
+  title: "Components/ToggleSwitch",
+  component: ToggleSwitch,
+};
+
+export const Default: StoryObj = {};
