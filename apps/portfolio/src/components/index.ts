c071474157baa0fe0c1cacb06f893d8fda7ea35d
etercy <suelengbr@gmail.com>
2024-02-09 19:53:43 -0300

    wip: saving progress

--- apps/portfolio/src/components/index.ts
+++ apps/portfolio/src/components/index.ts
@@ -4,3 +4,4 @@ export { Input } from './Input/input'
 export * as  Form from './Form/form'
 export { Toast } from "./toast/Toast";
 export * as Tag from './tag/Tag' 
+export { SidebarProfile } from './sidebar/SidebarProfile'
