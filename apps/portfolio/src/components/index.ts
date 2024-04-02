10feb82784cb84162dee05f69c352835d49432a9
etercy <suelengbr@gmail.com>
2024-04-02 14:17:48 -0300

    wip

--- apps/portfolio/src/components/index.ts
+++ apps/portfolio/src/components/index.ts
@@ -7,5 +7,3 @@ export * as Tag from "./tag/Tag";
 export * as Sidebar from "./sidebar/Sidebar";
 export * as Search from "./search/Search";
 export * as Select from "./select/Select";
-export * as Profile from "./profile";
-export * as Modal from "./modal/Modal";
