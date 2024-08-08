740f4f28a03c06627afc49fc6db38b93911c454d
etercy <suelengbr@gmail.com>
2024-08-08 10:58:34 -0300

    feat: project structure change

--- apps/zetflix/src/main.tsx
+++ apps/zetflix/src/main.tsx
@@ -1,11 +1,23 @@
+import { SessionProvider } from "./context/Session";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import "./main.css";
+import "./styles.css";
+
 import { router } from "./router";
+
+import { RouterProvider } from "react-router-dom";
+import { Loading } from "./components";
+
+const client = new QueryClient();
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <SessionProvider>
+      <QueryClientProvider client={client}>
+        <RouterProvider router={router} fallbackElement={<Loading />} />
+      </QueryClientProvider>
+    </SessionProvider>
   </StrictMode>
 );
